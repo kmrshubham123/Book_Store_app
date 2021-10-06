e026e6197a3369f8dfaaa068e869a3675bc0b233
kmrshubham123 <kmrshubham123@gmail.com>
2021-10-06 14:51:55 +0530

    [Kumar Shubham]Refactor:api integration and modification in UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './component/signup/signup.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomepageComponent } from './component/homepage/homepage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'signup' , component:SignupComponent},
+  {path:'login' , component:LoginComponent},
+  {path:'home' , component:HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
