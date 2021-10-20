5211621fab657e4e764fc994adf67ee2571765f9
kmrshubham123 <kmrshubham123@gmail.com>
2021-10-20 17:57:25 +0530

    [Kumar Shubham]Add:adminLogin api integration and UI

---
+++ src/app/component/adminlogin/adminlogin.component.spec.ts
@@ -0,0 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { AdminloginComponent } from './adminlogin.component';
+
+describe('AdminloginComponent', () => {
+  let component: AdminloginComponent;
+  let fixture: ComponentFixture<AdminloginComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminloginComponent ],
+      imports:[ MatSnackBarModule,
+                ReactiveFormsModule,
+                HttpClientModule,
+
+      ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminloginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
